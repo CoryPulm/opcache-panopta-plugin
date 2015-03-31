b3e62bd8af1c4dd43e5c7360888fb96572a03d85
Cory Pulm <corypulm@gmail.com>
2015-03-31 17:44:05 -0500

    completed for now, TODO: add conversion to MB

--- opcache.py
+++ opcache.py
@@ -1,17 +1,28 @@
 import agent_util
 import logging
-import httplib
+import urllib2
 
 logger = logging.getLogger(__name__)
 
 
-class UsersPlugin(agent_util.Plugin):
+class OpCachePlugin(agent_util.Plugin):
     textkey = "opcache"
     label = "PHP OpCache"
 
+    @classmethod
     def update_metrics(self, config):
-        r = httplib.HTTPConnection(self.url, 
-    
+        metric = {}
+        r = urllib2.urlopen(config['opcache_url'])
+        reply = r.read()
+        reply = reply.strip().strip(';').split(';')
+        for item in reply:
+            if 'hit_rate' in item or ('.php') in item: continue
+            else:
+                metric_name = item.split(':')[0]
+                metric_value = item.split(':')[-1]
+                metric["php_opcache." + metric_name] = float(metric_value)
+        return metric
+
     @classmethod
     def get_metadata(self, config):
         status = agent_util.MISCONFIGURED
@@ -19,35 +30,28 @@ class UsersPlugin(agent_util.Plugin):
 	
         if "opcache_url" in config:
             url = config['opcache_url']
-            stats = agent_util.SUPPORTED
+            status = agent_util.SUPPORTED
             msg = None
+        else:
+            return {}
+
+        metadata = {}
+        metrics = self.update_metrics(config)
 
         options = []
 
-        data = {
-            "php_opcache." + : {
-                "label": "Users unique login count",
-                "options": options,
-                "status": status,
-                "error_message": msg
-            },
-            "users.total_login_count": {
-                "label": "Users total login count",
-                "options": options,
-                "status": status,
-                "error_message": msg
-            },
-        }
-        return data
+        for textkey in metrics:
+            metadata[textkey] = {
+                    "label": textkey.replace('_',' ').title(),
+                    "options": options,
+                    "status": status,
+                    "error_message": msg
+                }
+        return metadata
 
     def check(self, textkey, data, config):
-        query = ''
-        if (textkey == 'users.unique_login_count'):
-            query = 'who | wc -l'
-
-        if (textkey == 'users.total_login_count'):
-            query = 'who |cut -c 1-9 |sort -u |wc -l'
-
-        ret, output = agent_util.execute_command('%s' % query)
-        return int(output)
-
+        tmp = self.update_metrics(config)
+        if textkey in tmp:
+            return tmp[textkey]
+        else:
+            return None
